1b82abe20b64627d2cacb5f0d4ab52acb1660400
mclovin <carapez31@gmail.com>
2024-07-15 22:00:35 -0600

    hot fix

--- src/main/java/com/aluracursos/forohub/model/Respuesta.java
+++ src/main/java/com/aluracursos/forohub/model/Respuesta.java
@@ -77,4 +77,59 @@ public class Respuesta {
         this.solucion = false;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public LocalDateTime getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDateTime fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public Boolean getSolucion() {
+        return solucion;
+    }
+
+    public void setSolucion(Boolean solucion) {
+        this.solucion = solucion;
+    }
+
+    public Usuario getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Usuario autor) {
+        this.autor = autor;
+    }
+
+    public Topico getTopico() {
+        return topico;
+    }
+
+    public void setTopico(Topico topico) {
+        this.topico = topico;
+    }
 }
